098fc45361f2f09f788b62093302350212de7a47
HenryCodeT <120686@unsaac.edu.pe>
2022-07-04 22:29:05 -0500

    add: arrays

--- Program.cs
+++ Program.cs
@@ -10,22 +10,38 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            int year = Convert.ToInt16("12");
-            double decimales = 2.3;
-            string nombre = "henry";
-            string nombres = "pedro vasquez";
-            decimal decimals = 2.07m;
-            float flotante = 8.9f;
-            bool booleano = false;
-            char caracter = 'R';
-            Decimal decimal1 = 24.5m;
+            // Tipo string
+            string[] cadenas = new string[5];
+            cadenas[0] = "Henry";
+            cadenas[1] = "Henry_1";
+            cadenas[2] = "Henry_2";
+            cadenas[3] = "Henry_3";
+            cadenas[4] = "Henry_4";
+            string[] name = { "pedrito" };
 
-            Console.WriteLine(nombres +" Hello Henry {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", year, decimales, nombre, decimals, flotante, booleano, caracter, decimal1);
-            
+            Console.WriteLine("Length", cadenas.Length);
+            Console.WriteLine(cadenas[0]);
+            Console.WriteLine(name[0]);
+
+            // Tipo int
+            int[] age = new int[5];
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            age[0] = 1;
+            age[1] = 2;
+            age[2] = 3;
+            age[3] = 4;
+            age[4] = 5;
+
+            Console.WriteLine("Edad ", age[0]);
+
+            // Arreglos mu
+            Console.WriteLine("*******************************************");
+            double[,] doble = new double[2, 2] { { 6, 3.7 }, { 6.3, 6.7 } };
+            Console.WriteLine( doble[0,1]);
+
+
             Console.ReadLine();
         }
-        // float 7 digitos 32 bits
-        // double 15-16 digitos 64 bits
-        // decimal 28-29 digitos 128 bits
     }
 }
